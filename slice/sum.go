d46122a1dfee097929efedb09139d566d26753ec
lu-sd <luhou.me@gmail.com>
2024-11-20 10:44:29 -0800

    slice sum test

--- slice/sum.go
+++ slice/sum.go
@@ -1,9 +1,22 @@
 package main
 
-func Sum(numbers [5]int) int {
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+// SumAll calculates the respective sums of every slice passed in.
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
