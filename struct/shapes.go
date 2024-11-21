b416cce2f5a707f8483bbf8a03b0b77ab3354450
lu-sd <luhou.me@gmail.com>
2024-11-21 10:13:08 -0800

    method test

--- struct/shapes.go
+++ struct/shapes.go
@@ -1,14 +1,20 @@
 package shape
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Height + rectangle.Width)
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
