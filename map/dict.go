e7ee184d33e01c0dcebba9b4d92247574b3d5abd
lu-sd <luhou.me@gmail.com>
2024-11-24 08:57:25 -0800

    refactory add function, add switch on err handle

--- map/dict.go
+++ map/dict.go
@@ -1,11 +1,22 @@
 package maps
 
-import "errors"
+const (
+	// ErrNotFound means the definition could not be found for the given word
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 
+	// ErrWordExists means you are trying to add a word that is already known
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+// DictionaryErr are errors that can happen when interacting with the dictionary.
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Dictionary store definitions to words.
 type Dictionary map[string]string
-
-// ErrNotFound means the definition could not be found for the given word.
-var ErrNotFound = errors.New("could not find the word you were looking for")
 
 // Search find a word in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
@@ -18,6 +29,17 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // Add inserts a word and definition into the dictionary.
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+
+	}
+
+	return nil
 }
