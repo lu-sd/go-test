5ee504b5bde954d158ba15ece71de59ea1d284ef
lu-sd <luhou.me@gmail.com>
2024-11-26 17:33:06 -0800

    add select and use channnel close()

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,23 @@
 package racer
 
-import (
-	"net/http"
-	"time"
-)
+import "net/http"
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	// struct{} is an empty struct, which occupies zero bytes of memory
+	// Using chan struct{} is a common pattern when you only need to signal an event (completion or readiness) without passing any data
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
