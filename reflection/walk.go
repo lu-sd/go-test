e20da3a66a8510a06952406d6c3a43dcd761b235
lu-sd <luhou.me@gmail.com>
2024-11-27 17:14:10 -0800

    encapsulate function

--- reflection/walk.go
+++ reflection/walk.go
@@ -3,12 +3,11 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-	if val.Kind() == reflect.Pointer {
-		val = val.Elem()
-	}
+	val := getValue(x)
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
+
 		switch field.Kind() {
 		case reflect.String:
 			fn(field.String())
@@ -16,4 +15,14 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
